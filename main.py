47e2a0b941d7210e0701550b660f52aae37e099a
eduardo furtado <eduardo.ozorio@aluno.senai.br>
2025-09-29 14:52:50 -0300

    cadrastando livro

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import sqlite3
 
+conexao = sqlite3.connect('biblioteca.db')
+cursor = conexao.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,)
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano_publicacao INTEGER,
+    disponivel text NOT NULL CHECK (disponivel IN ('sim', 'nao'))
 
 
+''')
+
+conexao.commit()
 
 
+def cadrastar_livro(titulo, autor, ano):
+    cursor.execute('''
+    INSERT INTO livros (titulo, autor, ano_publicacao, disponivel)
+    VALUES (?, ?, ?, 'sim')
+    ''', (titulo, autor, ano))
+    conexao.commit()
+    print(f'Livro "{titulo}" cadastrado com sucesso!')  
 
 
 
